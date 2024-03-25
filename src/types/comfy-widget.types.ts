8f0a62454d45ce311f4179deec8906f14caae526
SMTanimur <smtanimurrahman404@gmail.com>
2024-03-25 19:37:43 +0600

    Add reactflow package dependency

---
+++ src/types/comfy-widget.types.ts
@@ -0,0 +1,124 @@
+import { FlowPropsKey, Input } from "./comfy-props.types"
+export const NODE_GROUP = 'Group';
+export type WidgetKey = string
+/**
+ * Stable Diffusion Widget Interface
+ */
+export interface Widget {
+  name: WidgetKey
+  display_name?: string
+  description?: string
+  category: string
+  input: {
+    required: Record<PropertyKey, Input>,
+    optional?: Record<PropertyKey, Input>,
+  }
+  output: FlowPropsKey[]
+}
+
+export const UnknownWidget: Widget = {
+  name: "UNKNOWN_WIDGET",
+  display_name: "Unknown",
+  category: "Unknown",
+  input: { required: {} },
+  output: [],
+};
+
+export type Widgets = Record<WidgetKey, Widget>
+
+export const Widget = {
+  getDefaultFields(widget: Widget): Record<PropertyKey, any> {
+    const fields: Record<PropertyKey, any> = {}
+    for (const [key, input] of Object.entries(widget.input.required)) {
+      if (Input.isBool(input)) {
+        fields[key] = input[1].default ?? false
+      } else if (Input.isFloat(input)) {
+        fields[key] = input[1].default ?? 0.0
+      } else if (Input.isInt(input)) {
+        fields[key] = input[1].default ?? 0
+      } else if (Input.isString(input)) {
+        fields[key] = ''
+      } else if (Input.isList(input)) {
+        fields[key] = input[0][0]
+      }
+    }
+    return fields
+  },
+  findSeedFieldName(widget: Widget): string | undefined {
+    const inputs = widget.input.required;
+    if (inputs.seed) {
+      return 'seed'
+    }
+    if (inputs.noise_seed) {
+      return 'noise_seed'
+    }
+    return
+  },
+  isPrimitive(name: string): boolean {
+    if (name === "PrimitiveNode") {
+      return true;
+    }
+    const types = ["STRING", "BOOLEAN", "INT", "FLOAT"];
+    const type = name.split("_")[1];
+    return types.indexOf(type) >= 0;
+  }
+}
+
+export const specialWidgets = {
+  Note: {
+    "name": "Note",
+    "display_name": "Note",
+    "description": "Note",
+    "input": {
+      "required": {
+        "text": [
+          "STRING",
+          {
+            "multiline": true
+          }
+        ]
+      }
+    },
+    "output": [],
+    "category": "utils",
+  },
+  Group: {
+    "name": NODE_GROUP,
+    "display_name": NODE_GROUP,
+    "description": "Group",
+    "input": {
+      "required": {}
+    },
+    "output": [],
+    "category": "utils"
+  },
+  Primitive_STRING: createPrimitiveWidget("STRING"),
+  Primitive_BOOLEAN: createPrimitiveWidget("BOOLEAN"),
+  Primitive_INT: createPrimitiveWidget("INT"),
+  Primitive_FLOAT: createPrimitiveWidget("FLOAT"),
+  // Reroute: {
+  //   "name": "Reroute",
+  //   "input": {
+  //     "required": {}
+  //   },
+  //   "output": [],
+  //   "display_name": "Reroute",
+  //   "description": "Reroute",
+  //   "category": "utils",
+  // }
+}
+
+function createPrimitiveWidget(type: string) {
+  return {
+    "name": `Primitive_${type}`,
+    "input": {
+      "required": {}
+    },
+    "output": [
+      type
+    ],
+    "display_name": `Primitive ${type}`,
+    "description": `Primitive type of ${type}`,
+    "category": "utils",
+  }
+}
